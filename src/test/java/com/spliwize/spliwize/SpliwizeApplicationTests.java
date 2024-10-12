b80906e100376b6c79b3d7d444ce5400223b9d6e
ittzJonny <mananbansal162@gmail.com>
2024-10-12 19:03:38 +0530

    initialCommitSetup

---
+++ src/test/java/com/spliwize/spliwize/SpliwizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spliwize.spliwize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpliwizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
